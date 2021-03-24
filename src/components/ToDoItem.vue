cb150c38a7e9ec5078fd48ed20fc0829f0806bad
reggiegunawan <reggiegunawan88@gmail.com>
2021-03-24 20:31:37 +0700

    Delete to-do

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="container">
     <div class="row my-3 justify-content-between">
-      <h4>{{ todo.title }}</h4>
+      <h3 v-if="!editing">{{ todo.title }}</h3>
+      <input v-else type="text" class="col form-control mx-3" />
       <div class="div">
-        <button class="btn btn-success mx-3">Edit</button>
-        <button class="btn btn-danger">Delete</button>
+        <button @click="edit_toDo" class="btn btn-success mx-3">Edit</button>
+        <button @click="delete_toDo(todo.id)" class="btn btn-danger">
+          Delete
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   props: {
     todo: {},
+  },
+  data() {
+    return {
+      editing: false,
+    };
+  },
+  methods: {
+    ...mapActions(["delete_toDo"]),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -14,12 +14,23 @@ export default new Vuex.Store({
     add_toDo({ commit }, todo) {
       commit("add_toDo", todo);
     },
+    delete_toDo({ commit }, id) {
+      commit("delete_toDo", id);
+    },
+    edit_toDo({ commit }, name) {
+      commit("edit_toDo", name);
+    },
   },
   mutations: {
     add_toDo(state, todo) {
       state.toDoItems.push(todo);
+      localStorage.setItem("todolist_data", JSON.stringify(state.toDoItems));
       console.log(state.toDoItems);
     },
+    delete_toDo(state, id) {
+      state.toDoItems = state.toDoItems.filter((todo) => todo.id != id);
+    },
+    // edit_toDo(state, name) {},
   },
   modules: {},
 });
